9db7dd14c506ffe254683c5b139227ecacf272d8
ivrude <136907387+ivrude@users.noreply.github.com>
2023-06-24 01:42:06 +0300

    Update admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from .models import Task
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+    list_filter = ('user',)
+    search_fields = ('title', 'content')
+    fields = ('title', 'content', 'user')
+
+admin.site.register(Task, TaskAdmin)
+
